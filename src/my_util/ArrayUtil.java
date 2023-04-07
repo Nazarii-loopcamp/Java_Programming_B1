42f642ad3fd40aa81dede4edea90b830d0d21c32
skvozi <nazarii.duvanov@gmail.com>
2023-04-06 19:11:57 -0700

    day_26-work

--- src/my_util/ArrayUtil.java
+++ src/my_util/ArrayUtil.java
@@ -28,4 +28,77 @@ public class ArrayUtil {
 
     }
 
+    /**
+     * This method will accept an int array and the num
+     * It will return if the number exists in the array
+     */
+
+    public static boolean containArr (int [] arr, int num) { // 2, 4, 6, 8, 9, 10 | 4
+
+        for (int each : arr) {
+            if(each == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * This method accept int array and a number
+     * And returns the index of the number in the array
+     * If it does not exist it return -1
+     */
+    public static int indexOf (int [] arr, int num) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * This method accepts String array and a String
+     * And retrns the index of the String in the array
+     * If it does not exist, it returns -1
+     */
+    public static int indexOf (String [] arr, String word) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(word)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     *
+     * This method accepts int array and the int num
+     * And returns the a new array with the num added at the end
+     */
+    public static int [] add (int [] arr, int num) {
+
+        int [] newArr = Arrays.copyOf(arr, arr.length+1);
+        newArr[newArr.length-1] = num;
+
+        return newArr;
+
+    }
+
+    /**
+     *
+     * This method accepts String array and the String word
+     * And returns the new array with the word added at the end
+     */
+    public static String [] add (String [] arr, String word) {
+
+        String [] newArr = Arrays.copyOf(arr, arr.length+1);
+        newArr[newArr.length-1] = word;
+
+        return newArr;
+
+    }
+
 }
