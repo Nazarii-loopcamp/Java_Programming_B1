1de727169f79bbc255021129234355aa66053ffa
skvozi <nazarii.duvanov@gmail.com>
2023-04-15 12:09:36 -0700

    day_30_more Custom Classes

---
+++ src/day30_b_custom_classes/Person.java
@@ -0,0 +1,15 @@
+package day30_b_custom_classes;
+
+public class Person {
+
+    // These are instance variable
+    String name;
+    String lastName;
+    int age;
+    double height;
+    boolean isMarried;
+    public void test (){
+        System.out.println(name);
+    }
+
+}
